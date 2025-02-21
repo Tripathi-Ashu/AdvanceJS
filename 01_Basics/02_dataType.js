0131c56a3a60364a2fca320cb56f11a66b031958
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2025-02-21 13:29:40 +0000

    conversion of data type

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,14 @@
+// alert(3 + 3)  // we are using nodejs not react
+
+// number 
+//bigint 
+// string = "" 
+// boolean => true /false 
+// null => standlone value 
+// undefinfd =>  abhi value asing nahi hui hai 
+// symbol => unique data type 
+
+
+// obeject 
+
+console.log(typeof null); //object 
---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score = "33ab"
+
+let value = Number(score)
+
+console.log(typeof(value));
+console.log(value);               // Not a number NaN
+
+// 33ab --> NaN
+// null --> 0
+// nudefind --> NaN
+// true --> 1
+// hitesh --> NaN 
+
+// Boolean(1) --> true
+// Boolean("")--> false
+// boolean("ashutosh")--> true
+
