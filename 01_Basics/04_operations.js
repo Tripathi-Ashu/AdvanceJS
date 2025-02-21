294b7bf0ce9598807b80e82fe83171b68d01975e
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2025-02-21 13:47:44 +0000

    operator and comparision are done

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,20 @@
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 =str1+str2
+
+console.log("1"  + 2);      // 12
+console.log(1  + "2");      // 12
+console.log("1"  + 2 + 2);  // 122
+console.log(1  + 2 + "2");  // 32
+
+console.log(+true); // 2
+
+console.log(+"");  // 0
+
+let count = 100 
+count++
+console.log(count)    // 100
+
+
+
