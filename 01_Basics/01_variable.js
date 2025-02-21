82006bcdb28f3f46ea7036c35732d349805e8754
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2025-02-21 13:12:12 +0000

    variable

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 14434                             // locked Not allowed 
+let accountEmail = "Adhshbfdhdsb"
+var accountPassword = "12324"                      // Not Depend upon scope so never use
+
+accuCity = "Lucknow"                               // wrong way 
+
+
+
